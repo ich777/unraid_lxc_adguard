6c6002234764e2679c08c7d037d8d24723065ecd
Christoph Hummer <christophhummer@gmail.com>
2023-09-18 13:51:06 +0200

    Update 99-cleanup.sh

--- build/99-cleanup.sh
+++ build/99-cleanup.sh
@@ -3,3 +3,5 @@ apt clean
 rm -rf /var/cache/apt/archives/*
 rm -f /etc/ssh/*_key /etc/ssh/*.pub
 find /var/log -type f -mtime +30 -exec rm {} \;
+
+sed -i -e '/ go to http/c ***line removed***' /tmp/02-adguard.log
